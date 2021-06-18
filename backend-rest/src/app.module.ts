6b65cc25e8f20429e6722b1f4f3590a24ba20ae3
nakkrawee kuenkleep <nakkrawee@livingmobile.me>
2021-06-18 11:09:31 +0700

    install and config database

---
+++ backend-rest/src/app.module.ts
@@ -0,0 +1,23 @@
+import { Module } from '@nestjs/common';
+import { AppController } from './app.controller';
+import { AppService } from './app.service';
+import { SequelizeModule } from '@nestjs/sequelize';
+
+@Module({
+  imports: [
+    SequelizeModule.forRoot({
+      dialect: 'postgres',
+      host: 'localhost',
+      port: 5432,
+      username: 'postgres',
+      password: 'secret',
+      database: 'store-api',
+      autoLoadModels: true,
+      synchronize: true,
+      }),
+
+  ],
+  controllers: [AppController],
+  providers: [AppService],
+})
+export class AppModule {}
