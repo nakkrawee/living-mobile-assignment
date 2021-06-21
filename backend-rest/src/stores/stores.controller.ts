235d3c1b35a5cbb70010939d74e9bc4378123882
nakkrawee kuenkleep <nakkrawee@livingmobile.me>
2021-06-21 19:00:48 +0700

    add create store e2e test

--- backend-rest/src/stores/stores.controller.ts
+++ backend-rest/src/stores/stores.controller.ts
@@ -1,7 +1,7 @@
-import { Body, Controller, Delete, Get, Param, Post } from '@nestjs/common';
+import { Body, Controller, Delete, Get, Param, Post, Put } from '@nestjs/common';
 import { StoresService } from './stores.service';
 import { CreateStoreDto } from './dto/createStore.dto';
-import { ApiCreatedResponse, ApiOperation } from '@nestjs/swagger';
+import { ApiCreatedResponse, ApiOkResponse, ApiOperation } from '@nestjs/swagger';
 import { ApiBadRequestResponse } from '@nestjs/swagger';
 import { UsePipes } from '@nestjs/common';
 import { ValidationPipe } from '@nestjs/common';
@@ -11,33 +11,58 @@ import { ValidationPipe } from '@nestjs/common';
 export class StoresController {
    constructor(private readonly storesService: StoresService) {}
  
-   @Post()
-   @ApiOperation({ summary: 'Create store' })
-   create(@Body() createStoreDto: CreateStoreDto) {
-       return this.storesService.create(createStoreDto);
-   }
- 
-   @Get()
-   @ApiOperation({ summary: 'Find all store' })
-   findAll() {
-       return this.storesService.findAll();
-   }
- 
-   @Get(':id')
-   @ApiOperation({ summary: 'Find one store'})
-   findOne(@Param('id') id: string){
-       return this.storesService.findOne(id);
-   }
- 
-   @Delete(':id')
-   @ApiOperation({ summary: 'Delete store' })
-   remove(@Param('id') id: string) {
-       return this.storesService.remove(id);
-   }
- 
-   @Post(':id')
-   @ApiOperation({summary: 'Update store'})
-   update(@Param('id') id: string,@Body() createStoreDto: CreateStoreDto){
-       return this.storesService.update(id,createStoreDto)
-   }
+    @Post()
+    @ApiOperation({ summary: 'Create store' })
+    @ApiCreatedResponse({ 
+        description: 'The store has been successfully created.',
+        type: CreateStoreDto,
+        })
+    create(@Body() createStoreDto: CreateStoreDto) {
+        return this.storesService.create(createStoreDto);
+    }
+    
+    @Get()
+    @ApiOperation({ summary: 'Find all store' })
+    @ApiOkResponse({ 
+            description: 'All of store',
+            isArray: true,
+            type: CreateStoreDto,
+        })
+    findAll() {
+        return this.storesService.findAll();
+    }
+    
+    
+    @Get(':id')
+    @ApiOperation({ summary: 'Find one store'})
+    @ApiOkResponse({ 
+        description: 'Find store by id',
+        isArray: true,
+        type: CreateStoreDto,
+        })
+    findOne(@Param('id') id: string){
+        return this.storesService.findOne(id);
+    }
+    
+    @Delete(':id')
+    @ApiOperation({ summary: 'Delete store' })
+    @ApiOkResponse({ 
+        description: 'The store has been successfully deleted',
+        isArray: true,
+        type: CreateStoreDto,
+    })
+    remove(@Param('id') id: string) {
+        return this.storesService.remove(id);
+    }
+    
+    @Put(':id')
+    @ApiOperation({summary: 'Update store'})
+    @ApiOkResponse({ 
+        description: 'Update store by id',
+        isArray: true,
+        type: CreateStoreDto,
+        })
+    update(@Param('id') id: string,@Body() createStoreDto: CreateStoreDto){
+        return this.storesService.update(id,createStoreDto)
+    }
 }
